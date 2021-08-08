9bb6b4a5704353d86718d1fa3aba6915d79c4af4
lucasarundell <lucasarundell@geniesolutions.com.au>
2021-08-08 22:34:26 +1000

    add key to ssr head for docs

--- docs/gatsby-ssr.js
+++ docs/gatsby-ssr.js
@@ -5,6 +5,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <style
       id="stitches"
+      key="stitches-style"
       dangerouslySetInnerHTML={{
         __html: getCssString(),
       }}
